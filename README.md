c597efe39b5358b651a48c5014bcaa3b66ae6ecd
Thiago Maranhão <thiagofmaranhao@gmail.com>
2022-02-11 05:26:54 -0300

    add pubsub example 1
    - Creating solution
    - Adding pubsub example 1
    - Adding docker configuration instructions

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # nats.io
 Nats.io study repository
+
+## References
+- https://nats.io/
+- https://docs.nats.io/
+- https://youtube.com/playlist?list=PLfhPyyHRfeujNePekyCMqRvJoUehqAN05
+
+
+## Configure Docker
+```
+docker pull nats
+
+docker run --name nats -p 4222:4222 nats
+```
---
+++ src/Nats.Io.PubSub.Example1/Nats.Io.PubSub.Example1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using NATS.Client;
+
+namespace Nats.Io.PubSub.Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conexão
+            var factory = new ConnectionFactory();
+            var connection = factory.CreateConnection();
+            
+            // Subscription
+            EventHandler<MsgHandlerEventArgs> messageHandler = (sender, args) =>
+            {
+                Console.WriteLine($"{DateTime.Now:F} - Received: {args.Message}");
+            };
+
+            var subscribeAsync = connection.SubscribeAsync("foo");
+            subscribeAsync.MessageHandler += messageHandler;
+            subscribeAsync.Start();
+            
+            // Publish
+            var message = "Hello World!";
+            Console.WriteLine($"{DateTime.Now:F} - Send: {message}");
+            connection.Publish("foo", Encoding.UTF8.GetBytes(message));
+            
+            Thread.Sleep(1000);
+            
+            // Disconnect
+            subscribeAsync.Unsubscribe();
+            connection.Drain();
+            connection.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
